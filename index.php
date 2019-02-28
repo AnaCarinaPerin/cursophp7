981313bc7dcdb1e9485f09dee80489223f670c07
Ana <anacarinaperin@gmail.com>
2019-02-28 16:48:33 -0300

    Testes com a classe Usuario

--- index.php
+++ index.php
@@ -2,20 +2,27 @@
 
 require_once ("config.php");
 
-$lola = new Usuario();
+//Carrega uma lista de usuários buscando pelo login e a senha
+//$usuario = new Usuario();
+//$usuario->login("lola","senhadalola");
+//echo $usuario;
 
-$lola->loadById(5);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("lo");
+//echo json_encode($search);
 
-echo $lola;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-//Forma antes da classe Usuario.php
+//Carrega um usuário
+//$lola = new Usuario();
+//$lola->loadById(5);
+//echo $lola;
 
-/*
-$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
-*/
+//Mostrava os usuário antes da classe Usuario.php
+//$sql = new Sql();
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
 
 ?>
